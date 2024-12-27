969651d91208f04e36eef429006be3eb4002855d
DevBxx <dev.it@bionexx.com>
2024-12-27 17:41:18 +0300

    [ADD] ajout 27122024

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
+import "react-native-get-random-values";
+import "core-js/full/symbol";
+import "core-js/full/promise";
+import "core-js/full/url";
+import "core-js/web/immediate";
+import "core-js/features/abort-controller";
+import "core-js/features/array-buffer";
+import "core-js/features/structured-clone";
+import "core-js/web/timers";
+import "core-js/web/dom-collections";
 
-import App from './App';
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
